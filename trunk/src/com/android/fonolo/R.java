a73e069219e8e82544bc6d3617ad3213c695827f
johnthesaintjohn <johnthesaintjohn@9068d110-e1d2-11dd-9b7c-bbba4c9295f9>
2009-01-26 01:10:50 +0000

    Fixed the library issue

--- trunk/src/com/android/fonolo/R.java
+++ trunk/src/com/android/fonolo/R.java
@@ -27,8 +27,7 @@ public final class R {
         public static final int widget81=0x7f050008;
     }
     public static final class layout {
-        public static final int home=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
