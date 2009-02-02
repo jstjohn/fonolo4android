3376c4789afe0154f3b1e54f54d8c3c91e40c701
gardnerca <gardnerca@9068d110-e1d2-11dd-9b7c-bbba4c9295f9>
2009-02-02 23:05:53 +0000

    got floating help, most buttons work, company info screen works.

--- trunk/src/com/android/fonolo/R.java
+++ trunk/src/com/android/fonolo/R.java
@@ -14,29 +14,32 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int TextView_password=0x7f050015;
-        public static final int TextView_user=0x7f050014;
-        public static final int call_button=0x7f050004;
-        public static final int company_back=0x7f050001;
-        public static final int company_name_text=0x7f050002;
-        public static final int company_scroll=0x7f050006;
-        public static final int favorites_button=0x7f05000c;
-        public static final int favs_button=0x7f050003;
-        public static final int go_button=0x7f050016;
-        public static final int help_button=0x7f050008;
-        public static final int help_content=0x7f050007;
-        public static final int home_button=0x7f050005;
-        public static final int list_layout=0x7f050010;
-        public static final int list_scroll=0x7f050011;
-        public static final int login=0x7f050000;
-        public static final int new_search_button=0x7f050013;
-        public static final int output=0x7f05000f;
-        public static final int pass_field=0x7f05000e;
-        public static final int search_box=0x7f050009;
-        public static final int search_button=0x7f05000a;
-        public static final int search_text=0x7f05000b;
-        public static final int table_buttons=0x7f050012;
-        public static final int user_field=0x7f05000d;
+        public static final int TextView_password=0x7f050018;
+        public static final int TextView_user=0x7f050017;
+        public static final int call_button=0x7f050006;
+        public static final int call_screen=0x7f050000;
+        public static final int company_name_text=0x7f050004;
+        public static final int company_screen=0x7f050003;
+        public static final int company_scroll=0x7f050008;
+        public static final int end_call=0x7f050001;
+        public static final int favorites_button=0x7f05000f;
+        public static final int favs_button=0x7f050005;
+        public static final int go_button=0x7f050019;
+        public static final int help_button=0x7f05000b;
+        public static final int help_content=0x7f050009;
+        public static final int home_button=0x7f050007;
+        public static final int list_screen=0x7f050012;
+        public static final int list_scroll=0x7f050013;
+        public static final int login_screen=0x7f050016;
+        public static final int new_search_button=0x7f050015;
+        public static final int output=0x7f050002;
+        public static final int pass_field=0x7f050011;
+        public static final int search_box=0x7f05000c;
+        public static final int search_button=0x7f05000d;
+        public static final int search_screen=0x7f05000a;
+        public static final int search_text=0x7f05000e;
+        public static final int table_buttons=0x7f050014;
+        public static final int user_field=0x7f050010;
     }
     public static final class layout {
         public static final int call=0x7f030000;
