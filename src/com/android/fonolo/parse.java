eb11116e299b07b8a30fd5c0cb034ae474a83ad0
aabrsh <aabrsh@9068d110-e1d2-11dd-9b7c-bbba4c9295f9>
2009-02-02 03:05:15 +0000

    updated parse

--- src/com/android/fonolo/parse.java
+++ src/com/android/fonolo/parse.java
@@ -11,14 +11,15 @@ public class parse {
 	
 	//Parses through the results of a company_search() and returns a linkedlist of
 	//string arrays holding the company name in pos 0 and the company id in pos 1
-	public static LinkedList<String[]> parse_comp_search(JSONObject data){
-		
+	public static LinkedList<String[]> parse_comp_search(JSONObject result_json){
 		LinkedList<String[]> result = new LinkedList<String[]>();
-		int i = 0;
-		String number = "00"+Integer.toString(i);
-		while(data.has(number)){
-			JSONObject company;
-			try {
+		
+		try {
+			JSONObject data = result_json.getJSONObject("result").getJSONObject("data");
+			int i = 0;
+			String number = "00"+Integer.toString(i);
+			while(data.has(number)){
+				JSONObject company;
 				company = data.getJSONObject(number);
 				String name = company.getString("name");
 				String id = company.getString("id");
@@ -26,26 +27,59 @@ public class parse {
 				temp[0] = name;
 				temp[1] = id;
 				result.add(temp);
+				i++;
+				if(i < 10){
+					number = "00"+Integer.toString(i);
+				}else if(i < 100){
+					number = "0"+Integer.toString(i);
+				}else{
+					number = Integer.toString(i);
+				}
+				
+			}
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
 			
-			//increment i and reset the number to the next in sequence
-			//note: this will break if there are more than 999 companies 
-			//     in the list. Currently fonolo only returns 3 digits.
-			//     If fonolo changes this practice we need to update this.
-			i++;
-			if(i < 10){
-				number = "00"+Integer.toString(i);
-			}else if(i < 100){
-				number = "0"+Integer.toString(i);
-			}else{
-				number = Integer.toString(i);
+		return result;
+		
+	}
+	
+	
+	public static LinkedList<String[]> parse_comp_info(JSONObject result_json){
+		LinkedList<String[]> result = new LinkedList<String[]>();
+		
+		try {
+			JSONObject data = result_json.getJSONObject("result").getJSONObject("data");
+			int i = 0;
+			String number = "00"+Integer.toString(i);
+			while(data.has(number)){
+				JSONObject company;
+				company = data.getJSONObject(number);
+				String name = company.getString("name");
+				String id = company.getString("id");
+				String[] temp = new String[2];
+				temp[0] = name;
+				temp[1] = id;
+				result.add(temp);
+				i++;
+				if(i < 10){
+					number = "00"+Integer.toString(i);
+				}else if(i < 100){
+					number = "0"+Integer.toString(i);
+				}else{
+					number = Integer.toString(i);
+				}
+				
+			}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			
-		}
+			
 		return result;
 		
 	}
