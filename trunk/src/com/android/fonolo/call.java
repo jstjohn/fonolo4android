27d263cf09b112506b01bafd4840c65c24c42133
johnthesaintjohn <johnthesaintjohn@9068d110-e1d2-11dd-9b7c-bbba4c9295f9>
2009-02-06 00:26:41 +0000

    calls place successfully

--- trunk/src/com/android/fonolo/call.java
+++ trunk/src/com/android/fonolo/call.java
@@ -16,6 +16,7 @@ public class call extends Activity implements OnClickListener, private_constants
 	String uname = "";
 	String passwd = "";
 	//end copy------------------------------------------
+	TextView output;
 	TextView phone;
 	String id = "";
 	
@@ -26,7 +27,8 @@ public class call extends Activity implements OnClickListener, private_constants
         
         setContentView(R.layout.call);
         
-        TextView output = (TextView)this.findViewById(R.id.output);
+        output = (TextView)this.findViewById(R.id.output);
+        phone = (TextView)this.findViewById(R.id.my_phone_number);
         
         //copy into all classes--------------------------
 		Bundle extras = getIntent().getExtras();
@@ -38,9 +40,11 @@ public class call extends Activity implements OnClickListener, private_constants
 		String node_name = extras.getString("node_name");
 		String company_name = extras.getString("company_name");
 		
-		String outMessage = "Placing call to: "+company_name+"\n";
+		View call_button = this.findViewById(R.id.place_call);
+        call_button.setOnClickListener(this);
+		
+		String outMessage = "Company Name: "+company_name+"\n";
 		outMessage += "Destination: "+node_name+"\n";
-		outMessage += "Node ID: "+id;
 		
 		output.setText(outMessage);
 
@@ -55,13 +59,13 @@ public class call extends Activity implements OnClickListener, private_constants
 			String next3 = "";
 			String final4 = "";
 			if(raw_phone.length() == 11){
-				first3 = raw_phone.substring(1,3);
-				next3 = raw_phone.substring(4,6);
-				final4 = raw_phone.substring(7,10);
+				first3 = raw_phone.substring(1,4);
+				next3 = raw_phone.substring(4,7);
+				final4 = raw_phone.substring(7,11);
 			}else{
-				first3 = raw_phone.substring(0,2);
-				next3 = raw_phone.substring(3,5);
-				final4 = raw_phone.substring(6,9);
+				first3 = raw_phone.substring(0,3);
+				next3 = raw_phone.substring(3,6);
+				final4 = raw_phone.substring(6,10);
 			}
 			
 			try {
@@ -69,7 +73,11 @@ public class call extends Activity implements OnClickListener, private_constants
 					int response_code = result.getJSONObject("result").getJSONObject("head").getInt("response_code");
 					
 					if(response_code >= 200 && response_code < 300){
+						output.setText("Member phone number worked:"+first3+"-"+next3+"-"+final4);
 						JSONObject call_result = communication.call_start(id, first3+"-"+next3+"-"+final4, uname, passwd);
+						output.setText(call_result.getJSONObject("result").getJSONObject("head").getString("response_message"));
+					}else{
+						output.setText("Member phone number invalid: "+first3+"-"+next3+"-"+final4);
 					}
 				} catch (JSONException e) {
 					// TODO Auto-generated catch block
