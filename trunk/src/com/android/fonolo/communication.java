9d9ea9602ba831048d866042889dd41cf5cbd364
aabrsh <aabrsh@9068d110-e1d2-11dd-9b7c-bbba4c9295f9>
2009-02-09 01:39:21 +0000

    communication commented

--- trunk/src/com/android/fonolo/communication.java
+++ trunk/src/com/android/fonolo/communication.java
@@ -17,9 +17,18 @@ import org.json.JSONObject;
  * strings.
  *
  */
+
 public class communication {
 
-		//make it into a JSONObject
+		/*
+		 * Check member is the function which takes the user name and the password and check if the
+		 * account is exist and activated. 
+		 * The function takes the username and the password as strings, and store them into a
+		 * string array. the first element is the username, and the second element is the password.
+		 * Also, we need to pass the function name as a parameter in therequest.
+		 * The return is the "check_member" JSON request that going to be pass through fonolo_library 
+		 * to the fonolo DB.
+		 */
 		public static JSONObject check_member( String username, String password) throws JSONException
 		{
 			String[] params = new String[2];
@@ -30,6 +39,18 @@ public class communication {
 		} 
 		// End check member info function
 
+		/*
+		 * Check member number is the function which takes the user name, the password and the
+		 * member phone number, and check if the phone number is the one store under the user account.
+		 * The function takes the username, the password and the phone number as strings, and store 
+		 * them into a string array. the first element is the username,the second element is the
+		 *  password, and the third element is the phone number.
+		 *  Also, we need to pass the function name as a parameter in the JSON request.
+		 * The return is the "check_member_number" JSON request that going to be pass through 
+		 * fonolo_library to the fonolo DB. 
+		 */
+	
+		
 		public static JSONObject check_member_number( String username, String password, String phone) throws JSONException
 		{ 
 			String[] params = new String[3];
@@ -44,7 +65,17 @@ public class communication {
 
 		
 		// company search results
-		//
+
+		/*
+		 * Company search is the function which takes a search word, the user name and the password. 
+		 * The function takes the the search word, the username, the password as strings, and store 
+		 * the search word in a single element string array, and pass the username and the password as 
+		 * a parameters along with the array.
+		 *  Also, we need to pass the function name as a parameter in the request.
+		 * The return is the "company_search" JSON request that going to be pass through fonolo_library 
+		 * to the fonolo DB.		
+		 */
+		
 		public static JSONObject company_search(String searchstring, String user, String pass) throws JSONException
 		{
 			String[] params = new String[1];
@@ -56,8 +87,18 @@ public class communication {
 		// End company search results function
 
 		//
-		// lookup a company tree
-		//
+		// lookup a company 
+		/*
+		 * Company_details is the function which takes the company id, the user name and the password. 
+		 * The function takes the the company id, the username, the password as strings, and store 
+		 * the company id in a single element string array, and pass the username and the password as 
+		 * a parameters along with the array.
+		 *  Also, we need to pass the function name as a parameter in the request.
+		 * The return is the "company_details" JSON request that going to be pass through fonolo_library 
+		 * to the fonolo DB.		
+		 */
+		
+
 		public static JSONObject company_details( String treeid, String user, String pass) throws JSONException
 		{
 			String[] params = new String[1];
@@ -66,6 +107,20 @@ public class communication {
 			return make_json(json);
 		}
 		// End company lookup function
+		
+		/* ""This function is currently not in use, but it's going to be used in the next copy of the software"" 
+		
+		 * Company list is the function that list all the companies that stored in the fonolo DB.
+		 * this function takes the "limit number" which is the number of companies that you want to display 
+		 * per page. also, it takes the "page number" which is the number for the page that you are getting
+		 * the companies list from. Also, you can pass the date which is the date that you want to list
+		 * the companies that added to the DB after this date. The function also takes the username and password. 
+		 * The function will store the "limit", the "page number", and the date in a string array.
+		 * Then, we will pass the function name, the array, the username, and the password as a parameter in the request.
+		 * The return is the "company_list" JSON request that going to be pass through fonolo_library 
+		 * to the fonolo DB.		
+		 */
+		
 		public static JSONObject company_list( String limit, String pageno, String date, String user, String pass) throws JSONException
 		{ 
 			String[] params = new String[3];
@@ -77,6 +132,16 @@ public class communication {
 			return make_json(json);
 		} 
 		
+		
+		/*
+		 * Call start is the function which takes the user name, the password, the user phone number,
+		 * and the node id which is the id that has the direct to get to the option from the phone tree.
+		 * The function takes the node id and the user phone id as strings, and store them into a
+		 * string array. the first element is the node id, and the second is the user phone number.
+		 * Then, we pass the function name, the array, the username, and the password as a parameters in the request.
+		 * The return is the "caal_start" JSON request that going to be pass through fonolo_library 
+		 * to the fonolo to make the call.
+		 */
 		public static JSONObject call_start(String node_id, String user_phone_number, String user, String pass) throws JSONException{
 			String[] params = new String[2];
 			params[0] = node_id;
@@ -85,6 +150,14 @@ public class communication {
 			return make_json(json);
 		}
 		
+		/*
+		 * Call Cancel is the function which takes the user name, the password, and the session id which
+		 * is the id that can be found in the respond  from call start request.
+		 * The function takes the session id and store it in a single element string array. 
+		 * Then, we pass the function name, the array, the username, and the password as a parameters in the request.
+		 * The return is the "caal_cancel" JSON request that going to be pass through fonolo_library 
+		 * to the fonolo to cancel an existing call.
+		 */
 		public static JSONObject call_cancel(String session_id, String user, String pass) throws JSONException{
 			String[] params = new String[1];
 			params[0] = session_id;
@@ -92,6 +165,15 @@ public class communication {
 			return make_json(json);
 		}
 		
+
+		/*
+		 * Call status is the function which takes the user name, the password, and the session id which
+		 * is the id that can be found in the respond  from call start request.
+		 * The function takes the session id and store it in a single element string array. 
+		 * Then, we pass the function name, the array, the username, and the password as a parameters in the request.
+		 * The return is the "caal_cancel" JSON request that going to be pass through fonolo_library 
+		 * to the fonolo to check the status of an existing call.
+		 */
 		public static JSONObject call_status(String session_id, String user, String pass) throws JSONException{
 			String[] params = new String[1];
 			params[0] = session_id;
