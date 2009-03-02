91752193282533d0a28a17f94f4286d549761de8
johnthesaintjohn <johnthesaintjohn@9068d110-e1d2-11dd-9b7c-bbba4c9295f9>
2009-03-02 03:52:22 +0000

    Added source back...

---
+++ trunk/src/com/android/fonolo/private_constants.java
@@ -0,0 +1,22 @@
+package com.android.fonolo;
+
+/**
+ * 
+ * @author John St. John
+ * Last updated February 2009
+ * 
+ * This class holds some private constants that we need to give global access to
+ * within our program. We are also not commiting many of these comments,
+ *
+ *
+ */
+public interface private_constants {
+	//Provided by fonolo for THIS program. When distributing this file please
+	//delete the value below
+	// Type in your fonolo developer key below, then rename this file to private_constants.java
+	// be sure not to add it to the SVN repository!
+	static final String AUTH_KEY = "029d8219b07b5bf9b5256a2f4fcfbfb4"; 
+	static final int SEARCH_METHOD = 1;
+	static final int FAVS_METHOD = 2;
+	static final int LIST_METHOD = 3;
+}
