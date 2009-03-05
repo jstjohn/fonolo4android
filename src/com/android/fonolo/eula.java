86f1951ef2f059a31bf900532767d4e9d36f454d
gardnerca <gardnerca@9068d110-e1d2-11dd-9b7c-bbba4c9295f9>
2009-03-05 21:28:33 +0000

    eula page up

--- src/com/android/fonolo/eula.java
+++ src/com/android/fonolo/eula.java
@@ -19,18 +19,26 @@ import android.widget.TextView;
  */
 public class eula extends Activity implements OnClickListener{
 	TextView eula_content;
-	String content;
+	String message;	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		View eula_button = this.findViewById(R.id.accept_eula_button);
-		eula_button.setOnClickListener(this);
 		setContentView(R.layout.eula);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND,  
                 WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+		View eula_button = this.findViewById(R.id.accept_eula_button);
+		eula_button.setOnClickListener(this);
+		
+		message = "This software is provided \"as-is\". You agree that we are not " +
+				"liable for any damage caused through the use of this sofware. No " +
+				"warranty is given or implied. We are also not responsible for the " +
+				"actions of fonolo, or how they chose to use your information. By " +
+				"clicking accept you agree to all these terms, and that we are not " +
+				"to be held responsible for anything.";
 		
 		eula_content = (TextView)this.findViewById(R.id.eula_content);
+		eula_content.setText(message);
 		//getting bundled content
 		//Bundle extras = getIntent().getExtras();
 		//content = extras.getString("content");
